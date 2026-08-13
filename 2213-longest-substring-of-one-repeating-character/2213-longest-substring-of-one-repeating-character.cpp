4820cda35f13e060c71a84e4fc1f867f204e5db9
Manav Parashar <manavparashar26@gmail.com>
2026-08-13 15:58:25 +0530

    Time: 137 ms (64.43%), Space: 95.3 MB (81.88%) - LeetHub

--- 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.cpp
+++ 2213-longest-substring-of-one-repeating-character/2213-longest-substring-of-one-repeating-character.cpp
@@ -1,78 +1,55 @@
 class Solution {
 public:
     struct Node {
-        char lc, rc;
         int pref, suff, best, len;
-    };
+        char lc, rc;
+    }; 
 
-    vector<Node> seg;
-    string s;
-
-    Node merge(Node &L, Node &R) {
+    static Node merge(const Node& L, const Node& R) {
+        if (L.len == 0)
+            return R; 
+        if (R.len == 0)
+            return L;
         Node res;
         res.len = L.len + R.len;
         res.lc = L.lc;
         res.rc = R.rc;
-
         res.pref = L.pref;
         if (L.pref == L.len && L.rc == R.lc)
             res.pref = L.len + R.pref;
-
         res.suff = R.suff;
         if (R.suff == R.len && L.rc == R.lc)
             res.suff = R.len + L.suff;
-
         res.best = max(L.best, R.best);
         if (L.rc == R.lc)
             res.best = max(res.best, L.suff + R.pref);
-
         return res;
     }
 
-    void build(int idx, int l, int r) {
-        if (l == r) {
-            seg[idx] = {s[l], s[l], 1, 1, 1, 1};
-            return;
-        }
+    vector<Node> tree;
+    int sz;
 
-        int mid = (l + r) / 2;
-        build(idx * 2, l, mid);
-        build(idx * 2 + 1, mid + 1, r);
-
-        seg[idx] = merge(seg[idx * 2], seg[idx * 2 + 1]);
-    }
-
-    void update(int idx, int l, int r, int pos, char c) {
-        if (l == r) {
-            seg[idx] = {c, c, 1, 1, 1, 1};
-            return;
-        }
-
-        int mid = (l + r) / 2;
-
-        if (pos <= mid)
-            update(idx * 2, l, mid, pos, c);
-        else
-            update(idx * 2 + 1, mid + 1, r, pos, c);
-
-        seg[idx] = merge(seg[idx * 2], seg[idx * 2 + 1]);
-    }
-
-    vector<int> longestRepeating(string str, string queryCharacters,
-                                 vector<int>& queryIndices) {
-        s = str;
+    vector<int> longestRepeating(string s, string qc, vector<int>& qi) {
         int n = s.size();
-
-        seg.resize(4 * n);
-        build(1, 0, n - 1);
+        sz = 1;
+        while (sz < n)
+            sz <<= 1; 
+        tree.assign(2 * sz,
+                    Node{0, 0, 0, 0, 0, 0}); 
+        for (int i = 0; i < n; i++)
+            tree[sz + i] = {1, 1, 1, 1, s[i], s[i]};
+        for (int i = sz - 1; i >= 1; i--)
+            tree[i] = merge(tree[2 * i], tree[2 * i + 1]);
 
         vector<int> ans;
-
-        for (int i = 0; i < queryIndices.size(); i++) {
-            update(1, 0, n - 1, queryIndices[i], queryCharacters[i]);
-            ans.push_back(seg[1].best);
+        ans.reserve(qi.size());
+        for (size_t i = 0; i < qi.size(); i++) {
+            int idx = qi[i] + sz;
+            tree[idx] = {1, 1, 1, 1, qc[i], qc[i]};
+            for (idx >>= 1; idx >= 1; idx >>= 1)
+                tree[idx] = merge(tree[2 * idx], tree[2 * idx + 1]);
+            ans.push_back(tree[1].best);
         }
-
         return ans;
     }
 };
