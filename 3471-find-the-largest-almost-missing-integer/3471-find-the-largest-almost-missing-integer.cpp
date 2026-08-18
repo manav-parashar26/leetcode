8153096db06a5dab7d10308fdb71912ff6d68cf3
Manav Parashar <manavparashar26@gmail.com>
2026-08-18 13:42:22 +0530

    Time: 1 ms (62.41%), Space: 28.9 MB (84.21%) - LeetHub

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -2,25 +2,37 @@ class Solution {
 public:
     int largestInteger(vector<int>& nums, int k) {
         int n = nums.size();
-        unordered_map<int, int> windowCount; // value -> number of windows containing it
-        
-        for (int s = 0; s + k <= n; s++) {
-            unordered_set<int> seen;
-            for (int i = s; i < s + k; i++) {
-                seen.insert(nums[i]);
-            }
-            for (int v : seen) {
-                windowCount[v]++;
-            }
+
+        vector<int> freq(51, 0);
+
+        for (int x : nums) {
+            freq[x]++;
         }
-        
+        if (k == n) {
+            return *max_element(nums.begin(), nums.end());
+        }
+        if (k == 1) {
+            int ans = -1;
+
+            for (int x : nums) {
+                if (freq[x] == 1) {
+                    ans = max(ans, x);
+                }
+            }
+
+            return ans;
+        }
+
         int ans = -1;
-        for (auto& [val, cnt] : windowCount) {
-            if (cnt == 1) {
-                ans = max(ans, val);
-            }
+
+        if (freq[nums[0]] == 1) {
+            ans = max(ans, nums[0]);
         }
-        
+
+        if (freq[nums[n - 1]] == 1) {
+            ans = max(ans, nums[n - 1]);
+        }
+
         return ans;
     }
 };
