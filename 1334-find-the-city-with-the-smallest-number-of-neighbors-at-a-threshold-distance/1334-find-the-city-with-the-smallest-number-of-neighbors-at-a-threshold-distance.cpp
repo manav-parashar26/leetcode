a468e80d0200dd8e765f9a6d9fa7c612fce817b9
Manav Parashar <manavparashar26@gmail.com>
2026-09-16 23:31:30 +0530

    Time: 20 ms (28.74%), Space: 17.2 MB (49.45%) - LeetHub

--- 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ 1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1334-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        vector<vector<int>> dist(n,vector<int>(n,1e4));
-        for(auto i : edges){
+        vector<vector<int>> dist(n, vector<int>(n, 1e4));
+        for (auto i : edges) {
             dist[i[0]][i[1]] = i[2];
             dist[i[1]][i[0]] = i[2];
         }
-        for(int i = 0 ; i < n ; i++)dist[i][i] = 0;
-        for(int k = 0 ; k < n ; k ++){
-            for(int i = 0 ; i < n ; i++){
-                for(int j = 0 ; j < n ; j++){
-                    if(dist[i][k] == 1e4 || dist[k][j] == 1e4)continue;
-                    dist[i][j] = min(dist[i][j],dist[i][k] + dist[k][j]);
+        for (int i = 0; i < n; i++)
+            dist[i][i] = 0;
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][k] == 1e4 || dist[k][j] == 1e4)
+                        continue;
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
                 }
             }
         }
         int cntcity = n;
         int cityno = -1;
-        for(int i = 0 ; i < n ; i++){
-            int cnt = 0 ;
-            for(int j = 0 ; j < n ; j++){
-                if(dist[i][j] <= distanceThreshold)cnt++;
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                if (dist[i][j] <= distanceThreshold)
+                    cnt++;
             }
-            if(cnt <= cntcity){
+            if (cnt <= cntcity) {
                 cntcity = cnt;
                 cityno = i;
             }
