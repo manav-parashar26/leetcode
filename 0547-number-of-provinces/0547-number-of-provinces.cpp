7d59152e191f181d29be9329a190af5961d7ec54
Manav Parashar <manavparashar26@gmail.com>
2026-09-19 12:03:05 +0530

    Time: 0 ms (100%), Space: 19.4 MB (70.34%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,29 +1,59 @@
 class Solution {
-    void dfs(int node, vector<vector<int>>& isConnected, vector<int>& vis) {
-        vis[node] = 1;
+    // void dfs(int node, vector<vector<int>>& isConnected, vector<int>& vis) {
+    //     vis[node] = 1;
 
-        for (int j = 0; j < isConnected.size(); j++) {
-            if (isConnected[node][j] == 1 && !vis[j]) {
-                dfs(j, isConnected, vis);
-            }
-        }
-    }
+    //     for (int j = 0; j < isConnected.size(); j++) {
+    //         if (isConnected[node][j] == 1 && !vis[j]) {
+    //             dfs(j, isConnected, vis);
+    //         }
+    //     }
+    // }
 
 public:
+    vector<int> parent, rank;
+    int find(int x){
+        if(parent[x] ==  x)return x;
+
+        return parent[x] = find(parent[x]);
+    }
+    void unite(int u ,int v, int &cnt){
+        int pu = find(u);
+        int pv = find(v);
+        if(pu == pv)return;
+        if(rank[pu] < rank[pv]){
+            parent[pu] = pv;
+        }
+        else if(rank[pv] < rank[pu]){
+            parent[pv] = pu;
+        }else{
+            parent[pv] = pu;
+            rank[pu]++;
+        }
+        cnt--;
+    }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-
-        vector<int> vis(n, 0);
-
-        int cnt = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (!vis[i]) {
-                cnt++;
-                dfs(i, isConnected, vis);
+        parent.resize(n);
+        rank.resize(n,0);
+        int cnt = n;
+        for(int i = 0 ; i < n ; i++ )parent[i] = i;
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < n ; j++){
+                if(isConnected[i][j] == 1)unite(i,j,cnt);
             }
         }
-
         return cnt;
     }
 };
+        // vector<int> vis(n, 0);
+
+        // int cnt = 0;
+
+        // for (int i = 0; i < n; i++) {
+        //     if (!vis[i]) {
+        //         cnt++;
+        //         dfs(i, isConnected, vis);
+        //     }
+        // }
+
+        // return cnt;
