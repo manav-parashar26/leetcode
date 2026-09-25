8a62807158734d5cfca20cd77ed936c992a93395
Manav Parashar <manavparashar26@gmail.com>
2026-09-26 03:41:00 +0530

    Time: 3 ms (98.68%), Space: 10.2 MB (90.32%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int change(int amount, vector<int>& coins) {
-
-        const int LIMIT = INT_MAX;
-
-        vector<int> dp(amount + 1, 0);
+        vector<unsigned long long> dp(amount+1, 0);
         dp[0] = 1;
-
-        for (int coin : coins) {
-            for (int t = coin; t <= amount; t++) {
-
-                if (dp[t - coin] > LIMIT - dp[t])
-                    dp[t] = LIMIT;
-                else
-                    dp[t] += dp[t - coin];
+        for(int coin : coins) {
+            for(int i=coin;i<=amount;i++) {
+                dp[i] += dp[i-coin];
             }
         }
-
-        return dp[amount];
+        return (int)dp[amount];
     }
 };
